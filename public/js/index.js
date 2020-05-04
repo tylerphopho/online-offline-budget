2ab922914fd08417a9a241507b3e9f0f00ec0f48
tylerphopho <tyler.phopho@gmail.com>
2020-05-04 15:15:56 -0600

    created function to send transactions

--- public/js/index.js
+++ public/js/index.js
@@ -68,3 +68,77 @@ function populateChart() {
         }
     });
 }
+
+function sendTransaction(isAdding) {
+    let nameEl = document.querySelector("#t-name");
+    let amountEl = document.querySelector("#t-amount");
+    let errorEl = document.querySelector(".form .error");
+  
+    // validate form
+    if (nameEl.value === "" || amountEl.value === "") {
+      errorEl.textContent = "Missing Information";
+      return;
+    }
+    else {
+      errorEl.textContent = "";
+    }
+  
+    // create record
+    let transaction = {
+      name: nameEl.value,
+      value: amountEl.value,
+      date: new Date().toISOString()
+    };
+  
+    // if subtracting funds, convert amount to negative number
+    if (!isAdding) {
+      transaction.value *= -1;
+    }
+  
+    // add to beginning of current array of data
+    transactions.unshift(transaction);
+  
+    // re-run logic to populate ui with new record
+    populateChart();
+    populateTable();
+    populateTotal();
+    
+    // also send to server
+    fetch("/api/transaction", {
+      method: "POST",
+      body: JSON.stringify(transaction),
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {    
+      return response.json();
+    })
+    .then(data => {
+      if (data.errors) {
+        errorEl.textContent = "Missing Information";
+      }
+      else {
+        // clear form
+        nameEl.value = "";
+        amountEl.value = "";
+      }
+    })
+    .catch(err => {
+      // fetch failed, so save in indexed db
+      saveRecord(transaction);
+  
+      // clear form
+      nameEl.value = "";
+      amountEl.value = "";
+    });
+  }
+  
+  document.querySelector("#add-btn").onclick = function() {
+    sendTransaction(true);
+  };
+  
+  document.querySelector("#sub-btn").onclick = function() {
+    sendTransaction(false);
+  };
