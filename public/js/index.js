97b8083e4cfa270b52eab23b26486039ea76ac11
tylerphopho <tyler.phopho@gmail.com>
2020-05-04 15:14:38 -0600

    finished functions for displaying data

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 import { response } from "express";
 
 let transactions = []
-let theChart;
+let myChart;
 
 fetch("/api/transaction").then(response => {
     return response.json();
@@ -21,3 +21,50 @@ function populateTotal() {
     totalEl.textContent = total;
 }
 
+function populateTable() {
+    let tbody = document.querySelector("#tbody")
+    tbody.innerHTML = "";
+
+    transactions.forEach(transactions => {
+        let tr = document.createElement("tr");
+        tr.innerHTML = `
+        <td>${transaction.name}</td>
+        <td>${transaction.value}</td>
+      `;
+      tbody.append(tr);
+    });
+}
+
+function populateChart() {
+    let reversed = transactions.slice().reverse();
+    let sum = 0;
+
+    let labels = reversed.map(t => {
+        let date = new Date(t.date);
+        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    });
+
+    let data = reversed.map(t => {
+        sum += parseInt(t.value);
+        return sum;
+    });
+
+    // Removes any old charts that exists
+    if(myChart) {
+        myChart.destroy();
+    }
+
+    let ctx = document.getElementById("myChart").getContext("2d");
+    myChart = new Chart(ctx, {
+        type: "line",
+        data: {
+            labels,
+            datasets: [{
+                label: "Total Over Time",
+                fill: true,
+                backgroundColor: "#6666ff",
+                data
+            }]
+        }
+    });
+}
