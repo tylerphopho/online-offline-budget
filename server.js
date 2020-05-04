ccd9861a43f37c8783bbae311765b24f17d2c99e
tylerphopho <tyler.phopho@gmail.com>
2020-05-04 14:54:53 -0600

    installed path and setup server.js

--- server.js
+++ server.js
@@ -17,12 +17,18 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
-});
+  useFindAndModify: false,
+  useUnifiedTopology: true
+},
+() => console.log("Connected to MongoDB!")
+);
 
 // routes
 app.use(require("./routes/api.js"));
+app.get("/", (req,res ) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port http://localhost:${PORT}!`);
 });
